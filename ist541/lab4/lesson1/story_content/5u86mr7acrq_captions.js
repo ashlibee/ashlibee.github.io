10e6988a19c3acadfd80f80d7669472ae250450d
ashlibee <89711633+ashlibee@users.noreply.github.com>
2021-09-25 13:09:38 -0700

    lesson1

--- ist541/lab4/lesson1/story_content/5u86mr7acrq_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.11.23355.0%0D%0A%0D%0A00:00:00.032%20--%3E%2000:00:03.840%0D%0ALet\u0027s%20check%20the%20next%20heading%20on%20the%20page.%20Click%20on%20%22How%20to%20Plant.%22%0D%0A%0D%0A"}')
--- ist541/lab4/lesson1/story_content/5z89QQ1QAn6_captions.js
+++ ist541/lab4/lesson1/story_content/5z89QQ1QAn6_captions.js
@@ -1,1 +1,1 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.11.23355.0%0D%0A%0D%0A00:00:00.325%20--%3E%2000:00:03.836%0D%0ASelect%20%22Apply%22%20to%20make%20the%20change.%0D%0A%0D%0A"}')
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.11.23355.0%0D%0A%0D%0A00:00:00.045%20--%3E%2000:00:03.188%0D%0ASelect%20%22Apply%22%20to%20make%20the%20change.%0D%0A%0D%0A"}')

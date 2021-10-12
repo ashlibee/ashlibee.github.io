32f126527ba892c72a16e8b148726c6ca5d22b04
ashlibee <89711633+ashlibee@users.noreply.github.com>
2021-10-11 19:58:13 -0700

    lab6

---
+++ ist541/lab6/js/hotspot.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+
+$( function() {
+   $("map area").click( function(){
+
+      if ($(this).attr("id") == "Maple Leaf") {
+         $("#feedback").css("color","red");
+         $("#feedback").html("No, but maple trees give us sweet maple syrup!");
+      }
+
+      if ($(this).attr("id") == "Oak Leaf") {
+         $("#feedback").css("color","green");
+         $("#feedback").html("Yes, oak trees produce acorns!");
+      }
+
+      if ($(this).attr("id") == "Birch Leaf") {
+         $("#feedback").css("color","red");
+         $("#feedback").html("No, birch trees don't produce acorns.");
+      }
+
+      if ($(this).attr("id") == "Ash Leaf") {
+         $("#feedback").css("color","red");
+         $("#feedback").html("No, ash trees don't produce acorns.");
+      }
+  $("#feedback").css("backgroundColor","yellow");
+$('#myModal').modal("show");
+
+   });
+}); //end main jQuery function

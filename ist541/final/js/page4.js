d37ef160d953e0954360a30b3250b240f3d27359
ashlibee <89711633+ashlibee@users.noreply.github.com>
2021-10-18 21:23:32 -0700

    101821

--- ist541/final/js/page4.js
+++ ist541/final/js/page4.js
@@ -4,17 +4,17 @@ $( function() {
 
       if ($(this).attr("id") == "Near Step 2") {
          $("#feedback").css("color","green");
-         $("#feedback").html("Yes, that would place the image near the step it corresponds to, step 2.");
+         $("#feedback").html("✅ Yes, that would place the image near the step it corresponds to, step 2.");
       }
 
       if ($(this).attr("id") == "paragraph text") {
          $("#feedback").css("color","red");
-         $("#feedback").html("No, that doesn't move the image closer to the step it corresponds to, step 2.");
+         $("#feedback").html("❌ No, that doesn't move the image closer to the step it corresponds to, step 2.");
       }
 
       if ($(this).attr("id") == "Steps 4-5") {
          $("#feedback").css("color","red");
-         $("#feedback").html("No, this would place the image with steps it doesn't correspond to. ");
+         $("#feedback").html("❌ No, this would place the image with steps it doesn't correspond to. ");
       }
  
   $("#feedback").css("backgroundColor","yellow");

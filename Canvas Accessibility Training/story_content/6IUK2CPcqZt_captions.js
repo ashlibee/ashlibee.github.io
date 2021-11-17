0ce20dda63b666da93cc49bb961b5e2e567e900f
ashlibee <89711633+ashlibee@users.noreply.github.com>
2021-11-16 22:18:20 -0800

    access

---
+++ Canvas Accessibility Training/story_content/6IUK2CPcqZt_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.11.23355.0%0D%0A%0D%0A00:00:01.272%20--%3E%2000:00:04.238%0D%0AAnd%20then%20save%20your%20page%20to%20finalize%20your%20changes.%0D%0A%0D%0A"}')
